20dbf2b0893e8136e3a41d20332446f665a4f4cf
Floris Buwalda <buwalda@v-slurmsub001.directory.intra>
2023-09-21 13:31:26 +0200

    added module load petsc

--- build_alma8.sh
+++ build_alma8.sh
@@ -7,11 +7,13 @@ source /opt/apps/intelmpi/2021.10.0/setvars.sh
 module load netcdf/4.9.2_4.6.1_intel2023.1.0
 module load gdal/3.6.3_intel2023.1.0
 module load gcc/12.2.0_gcc12.2.0
+module load petsc/3.19.5_intel2023.2.1
+
 #      . $SETVARS_VARS_PATH -ofi_internal=1
      export FC=mpiifort
      export CXX=mpiicx
      export CC=mpiicc
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/u/buwalda/petsc/arch-linux-c-opt/lib/pkgconfig/
+
 #rm -r -f build_all
 cmake ./src/cmake -DCMAKE_CXX_COMPILER=mpicxx -D CONFIGURATION_TYPE:STRING=All -D CMAKE_BUILD_TYPE=Release -B build_all
 
