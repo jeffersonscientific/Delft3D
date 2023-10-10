ffc3b6b888b5929508785aba346b41a5736621de
buwalda <floris.buwalda@deltares.nl>
2023-10-10 16:22:20 +0200

    Removed commented #IF(win32) and fixed indentation

--- src/cmake/configurations/swan_configuration.cmake
+++ src/cmake/configurations/swan_configuration.cmake
@@ -4,11 +4,9 @@ add_subdirectory(${checkout_src_root}/${swan_mpi_module} swan_mpi)
 add_subdirectory(${checkout_src_root}/${swan_omp_module} swan_omp)
 
 # netcdf
-#if(WIN32)
-    if(NOT TARGET netcdff)
-        add_subdirectory(${checkout_src_root}/${netcdf_module} netcdff)
-    endif()
-#endif()
+if(NOT TARGET netcdff)
+    add_subdirectory(${checkout_src_root}/${netcdf_module} netcdff)
+endif()
 
 # Project name must be at the end of the configuration: it might get a name when including other configurations and needs to overwrite that
 project(swan)
