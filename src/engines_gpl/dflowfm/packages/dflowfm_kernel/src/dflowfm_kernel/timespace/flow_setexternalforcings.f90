d9c05f7884effc5e7083f720c080f60f1eba523e
Bea Saggiorato <beatrice.saggiorato@deltares.nl>
2023-10-10 16:36:20 +0200

    UNST-7271: converted wave direction to cartesian convention when jawave==7

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/flow_setexternalforcings.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/timespace/flow_setexternalforcings.f90
@@ -542,6 +542,11 @@ subroutine set_wave_parameters()
          success = .true.
       end if
       
+      ! convert wave direction from nautial to cartesian convention
+      if(jawave == 7) then
+          phiwav = modulo(270d0 - phiwav,360d0)
+      end if
+      
       all_wave_variables = .not.(jawave == 7 .and. waveforcing /=3)
       call select_wave_variables_subgroup(all_wave_variables)
       
