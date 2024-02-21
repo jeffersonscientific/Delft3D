fb41bc9cde614972941a65e80d9b065e03e1aec0
Matthijs Huisman <matthijs.huisman@deltares.nl>
2024-02-21 11:01:46 +0100

    Also run the new test from the dockerfile

--- test/deltares_testbench/test/tools/test_failed_tests.py
+++ test/deltares_testbench/test/tools/test_failed_tests.py
@@ -5,9 +5,9 @@ from tools.failed_tests import get_failed_tests
 class TestFailedTests:
     def test_get_failed_tests(self, tmpdir):
         # Arrange
-        expected_result = ['test1', 'test3']
+        expected_result = ["test1", "test3"]
         test_log = tmpdir.join("test_log.log")
-        with open(str(test_log), 'a') as f:
+        with open(str(test_log), "a") as f:
             f.write("|test1|INFO|ERROR\n")
             f.write("|test2|INFO|SUCCESS\n")
             f.write("|test3|INFO|ERROR\n")
