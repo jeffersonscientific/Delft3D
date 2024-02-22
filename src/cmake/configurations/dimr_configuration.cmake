fad9d5563aa556d9c8678bb24aa3c784e25d39c4
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-02-22 10:50:44 +0100

    added expat module to dimr

--- src/cmake/configurations/dimr_configuration.cmake
+++ src/cmake/configurations/dimr_configuration.cmake
@@ -7,13 +7,6 @@ if(NOT TARGET deltares_common_c)
   add_subdirectory(${checkout_src_root}/${deltares_common_c_module} deltares_common_c)
 endif()
 
-# for unix, we need to build expat xml parser
-if(UNIX)
-  if(NOT TARGET expat)
-    add_subdirectory(${checkout_src_root}/${expat_module} expat)
-  endif()
-endif(UNIX)
-
 # DIMR specific components
 add_subdirectory(${checkout_src_root}/${dimr_lib_module} dimr_lib)
 add_subdirectory(${checkout_src_root}/${dimr_module} dimr)
@@ -21,5 +14,8 @@ add_subdirectory(${checkout_src_root}/${dimr_module} dimr)
 # install
 add_subdirectory(${checkout_src_root}/${install_dimr_module} install_dimr)
 
+if(${configuration_type} STREQUAL ${dimr_configuration})
+   add_subdirectory(${checkout_src_root}/${expat_module} expat)
+endif()
 # Project name must be at the end of the configuration: it might get a name when including other configurations and needs to overwrite that
 project(dimr)
