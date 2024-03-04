d3947974b723e06449c68c338eab1cff80a81cc8
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-03-04 17:17:41 +0100

    changes in response to review comments

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -12,7 +12,5 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.c
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/windows_postbuild_configuration.cmake)
 
-# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tools_gpl_configuration.cmake)
-
 # Project name must be at the end of the configuration: it might get a name when including other configurations and needs to overwrite that
 project(all)
