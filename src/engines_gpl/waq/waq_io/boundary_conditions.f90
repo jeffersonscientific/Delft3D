190d599a64d345c7ad2ba8dc22cef65ced805248
Mostafa Farrag <moah.farag@gmail.com>
2024-03-25 11:43:53 +0100

    move dlwq5g subroutine to boundary_condition module

--- src/engines_gpl/waq/waq_io/boundary_conditions.f90
+++ src/engines_gpl/waq/waq_io/boundary_conditions.f90
@@ -25,7 +25,6 @@ module boundary_conditions
     use timers
     use m_string_utils
     use waq_timers, only : read_time_delay
-    use m_dlwq5g
     use matrix_utils, only : assign_matrix
     use m_dlwq5d
     use m_dlwq5c
@@ -481,7 +480,7 @@ contains
                 goto 510
             endif
             ! Checks if an inner loop column header exists for the data matrix
-            call dlwq5g (lunut, int_workspace, itmnr, count_items_in_use_rule, idmnr, &
+            call validate_column_headers (lunut, int_workspace, itmnr, count_items_in_use_rule, idmnr, &
                     nodim, iorder, max_int_size, char_arr, iposr, &
                     npos, ilun, lch, lstack, cchar, &
                     chulp, nocol, is_date_format, is_yyddhh_format, itfacw, &
@@ -1012,5 +1011,157 @@ contains
         if (timon) call timstop(ithndl)
 
     end subroutine write_breakpoint_data_blocks
+
+    subroutine validate_column_headers(ascii_output_file_unit, i_array, count_items_assign, count_items_comp_rule, count_subs_assign, &
+            count_subs_comp_rule, index_first, i_max, names_to_check, start_in_line, &
+            npos, ilun, lch, lstack, cchar, &
+            chulp, nocol, is_date_format, is_yyddhh_format, itfact, &
+            itype, ihulp, rhulp, error_idx, status)
+
+        ! Checks if column header exists
+        ! Logical Units : LUN(27) = unit stripped DELWAQ input file
+        !                 LUN(29) = unit formatted output file
+        !
+        !     ascii_output_file_unit                  integer        1            input   unit number for ascii output
+        !     i_array                integer      i_max          in/out  integer workspace array
+        !     i_max                  integer        1            input   max. integer workspace dimension
+        !     count_items_assign     integer        1            in/out  number of items for assignment
+        !     count_items_comp_rule  integer        1            in      number of items in computational rule
+        !     count_items_entries    integer        1            in      number of items entries to be filled
+        !     count_subs_assign      integer        1            in/out  number of subst for assignment
+        !     count_subs_comp_rule   integer        1            in      number of subst in computational rule
+        !     count_subs_entries     integer        1            in      number of subst entries to be filled
+        !     index_first            integer        1            in      1 = items first, 2 = substances first
+        !     names_to_check         char*(*)      count_names   input   names of items to check for presence
+        !     count_names            integer        1            in/out  start position on input line
+        !     start_in_line          integer        1            in/out  start position on input line
+        !     npos                   integer        1            input   number of significant characters
+        !     ilun                   integer       lstack        input   unitnumb include stack
+        !     lch                    char*(*)      lstack        input   file name stack, 4 deep
+        !     lstack                 integer        1            input   include file stack size
+        !     cchar                  char*1         1            input   comment character
+        !     chulp                  char*(*)       1            output  space for limiting token
+        !     nocol                  integer        1            output  number of collums in matrix
+        !     is_date_format                 logical        1            input   true if time in 'date' format
+        !     is_yyddhh_format                 logical        1            input   true if yyetc instead of ddetc
+        !     itfact                 integer        1            input   factor between clocks
+        !     itype                  integer        1            output  type of info at end
+        !     ihulp                  integer        1            output  parameter read to be transferred
+        !     rhulp                  real           1            output  parameter read to be transferred
+        !     error_idx              integer        1            output  error index within current subroutine
+        !     offset_i_array         integer        1            output  offset  in i_array
+        !     offset_names           integer        1            output  offset in names_to_check
+        !     offset_common          integer        1            output  comon offset in i_array and names_to_check
+
+        use usefor, only : compact_usefor_list
+        use timers       !   performance timers
+        use date_time_utils, only : convert_string_to_time_offset, convert_relative_time
+
+        integer(kind = int_wp) :: i_max
+        character*(*) lch   (lstack), chulp, names_to_check(:)
+        character     cchar*1, strng*8
+        dimension     i_array(:), ilun(lstack)
+        logical       is_date_format, is_yyddhh_format, first, must_read_more
+        integer(kind = INT64) :: ihulp8
+        integer(kind = int_wp) :: ithndl = 0
+        integer(kind = int_wp) :: i, count_items_comp_rule, count_subs_assign, count_subs_comp_rule, index_first, offset_names
+        integer(kind = int_wp) :: offset_common, notim
+        integer(kind = int_wp) :: itype, ascii_output_file_unit, ilun, start_in_line, nopos, ihulp, error_idx
+        integer(kind = int_wp) :: i_array, nocol, ifound, itfact, icnt, iods, k
+        integer(kind = int_wp) :: offset_i_array, count_items_assign, count_names, npos, lstack
+        real(kind = real_wp) :: rhulp
+
+        type(error_status), intent(inout) :: status !< current error status
+
+        if (timon) call timstrt("validate_column_headers", ithndl)
+
+        ! Array offsets
+        offset_i_array = count_items_assign + count_items_comp_rule + &
+                count_subs_assign + count_subs_comp_rule
+        if (index_first == 1) then ! items first
+            offset_names = count_items_assign + count_items_comp_rule + count_subs_assign
+            offset_common = count_items_assign + count_items_comp_rule
+            count_names = count_subs_comp_rule
+        else if (index_first == 2) then !substances first
+            offset_names = count_items_assign + count_subs_comp_rule + count_subs_assign
+            offset_common = count_subs_comp_rule + count_subs_assign
+            count_names = count_items_comp_rule
+        endif
+
+        ! read loop
+        first = .true.
+        must_read_more = .true.
+        do while (must_read_more)
+            itype = 0
+            call rdtok1 (ascii_output_file_unit, ilun, lch, lstack, cchar, &
+                    start_in_line, npos, chulp, ihulp, rhulp, &
+                    itype, error_idx)
+
+            if (error_idx  /= 0) then ! error occurred when reading
+                if (timon) call timstop(ithndl)
+                return !exit subroutine
+            end if
+
+            ! no error
+            if (itype == 1) then ! a string has arrived
+                call convert_string_to_time_offset (chulp, ihulp, .false., .false., error_idx)
+                if (error_idx == 0) then
+                    error_idx = -2
+                    if (first) then
+                        if (timon) call timstop(ithndl)
+                        return  !exit subroutine
+                    else
+                        exit
+                    endif
+                endif
+                if (first) then
+                    first = .false.
+                    do i = 1, count_names
+                        i_array(offset_i_array + i) = 0
+                    end do
+                    nocol = 0
+                    write (ascii_output_file_unit, *)
+                endif
+                nocol = nocol + 1
+                strng = 'NOT used'
+                do i = 1, count_names
+                    if (string_equals(chulp(:20), names_to_check(offset_names + i))) then
+                        strng = 'used'
+                        i_array(i + offset_i_array) = nocol
+                    endif
+                end do
+                write (ascii_output_file_unit, 1000) nocol, chulp, strng
+            else
+                if (itype == 2) then ! an integer has arrived
+                    call convert_relative_time (ihulp, itfact, is_date_format, is_yyddhh_format)
+                endif
+                error_idx = -1
+                must_read_more = .false.
+                if (first) then
+                    if (timon) call timstop(ithndl)
+                    return !exit subroutine
+                end if
+            endif
+        end do
+
+        ! is everything resolved ?
+        icnt = 0
+        iods = 0
+
+        do i = 1, count_names
+            k = i - icnt
+            if ((names_to_check(offset_names + k) /= '&$&$SYSTEM_NAME&$&$!') &
+                    .and.  (i_array(offset_i_array + k) <= 0)) then
+                call compact_usefor_list(ascii_output_file_unit, i_array, count_items_assign, &
+                        count_items_comp_rule, count_subs_assign, &
+                        count_subs_comp_rule, index_first, names_to_check, &
+                        offset_i_array, offset_names, &
+                        iods, offset_common, k, icnt, error_idx, status)
+            end if
+        end do
+
+        1000 FORMAT (' Column:', I3, ' contains: ', A40, ' Status: ', A8)
+
+    end subroutine validate_column_headers
 
 end module boundary_conditions
