4563832a96e50159ba3ced27ac4b82c8a01d0f17
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-05-14 14:06:33 +0200

    remove_set_fillvalue

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/unc_write_his.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_io/unc_write_his.F90
@@ -1637,25 +1637,4 @@ subroutine write_station_netcdf_variable(i_his_file, output_variable_item)
    call check_netcdf_error( nf90_put_var(ihisfile, local_id_var, transformed_data, count = counts, start = starts))
 end subroutine write_station_netcdf_variable
 
-!> set the fill value according to dmiss/intmiss of a netcdf variable and the requested precision
-subroutine set_fill_value(ncid, varid, nc_precision)
-
-integer, intent(in   ) :: ncid         !< file id of the open netcdf dataset that varid was defined in
-integer, intent(in   ) :: varid        !< nf90 variable id of the variable that needs a fill value
-integer, intent(in   ) :: nc_precision !< precision of the variable that needs a fill value
-
- ! Add a fill value of the correct type
-   select case (nc_precision)
-   case (nf90_short, nf90_int)   
-      call check_netcdf_error( nf90_put_att(ncid, varid, '_FillValue', intmiss))
-   case (nf90_float) 
-      call check_netcdf_error( nf90_put_att(ncid, varid, '_FillValue', real(dmiss)))
-   case (nf90_double)
-      call check_netcdf_error( nf90_put_att(ncid, varid, '_FillValue', dmiss))
-   case default
-      call mess(LEVEL_ERROR,'nf90_put_att: invalid netcdf type for fill_value!')
-   end select
-   
-end subroutine set_fill_value
-
 end subroutine unc_write_his
