ab226378e263e06ed9df60209140aa91ed42be88
Tijn Berends <tijn.berends@deltares.nl>
2024-05-15 09:42:21 +0200

    UNST-7957 revert erroneous code change
    That.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_utils/rest_f90/find_crossed_links_kdtree2.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_utils/rest_f90/find_crossed_links_kdtree2.f90
@@ -190,7 +190,11 @@
          dlinlen = dbdistance(xa,ya,xb,yb, jsferic, jasfer3D, dmiss)
 
 !        determine square search radius
-         R2search = 1.1d0*(dlinlen+dmaxpollen)**2  ! 1.1d0: safety
+         if ( jsferic.eq.0 ) then
+            R2search = 1.1d0*(dlinlen+dmaxpollen+2d0*dtol)**2  ! 1.1d0: safety
+         else
+            R2search = 1.1d0*(dlinlen+dmaxpollen+2d0*dtol*Ra)**2  ! 1.1d0: safety
+         end if
 
 !        count number of points in search area
          NN = kdtree2_r_count(treeinst%tree,treeinst%qv,R2search)
