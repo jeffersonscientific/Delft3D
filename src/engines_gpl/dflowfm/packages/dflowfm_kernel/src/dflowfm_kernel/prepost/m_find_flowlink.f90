065131d323b985e346373a8959760589e397b963
Tijn Berends <tijn.berends@deltares.nl>
2024-05-16 11:49:06 +0200

    UNST-4870 find nearest flowlink using k-d tree
    find_flowlinks_kdtree uses the k-d tree routines to find the nearest flowlinks to a set of x,y-coordinates
    
    Tested in a small model set-up. It gives the same results as find_flowlinks_bruteforce (except in rare cases where two flowlinks have the exact same perpendicular distance to the point, meaning there is no unique solution), but it is generally 3 - 4 times faster (tested for 100, 10000, and 1000000 points on a grid with ~ 750 flowlinks).

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_find_flowlink.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/m_find_flowlink.f90
@@ -37,6 +37,110 @@ module m_find_flowlink
    
    contains
    
+   !> Find the flowlinks with the shortest perpendicular distances to the points [xx,yy]
+   !! Uses the k-d tree routines
+   subroutine find_flowlinks_kdtree(xx, yy, link_ids_closest)
+      use stdlib_kinds, only: dp
+      use MessageHandling, only: mess, LEVEL_ERROR
+      use m_flowgeom, only: lnx, ln, xz, yz
+      use m_alloc, only: realloc
+      use kdtree2Factory, only: kdtree_instance, find_nearest_sample_kdtree
+      use geometry_module, only: dlinedis
+      use m_sferic, only: jsferic, jasfer3D
+      use m_missing, only: dmiss
+      
+      real(dp), dimension(:), intent(in   ) :: xx, yy           !< x,y-coordinates
+      integer,  dimension(:), intent(  out) :: link_ids_closest !< ids of the flowlinks whose midpoints lie closest to the points [xx,yy]
+      
+      real(dp), dimension(lnx)             :: flowlink_midpoints_x
+      real(dp), dimension(lnx)             :: flowlink_midpoints_y
+      integer                              :: link_id, ka, kb
+      integer                              :: number_of_points, i_point
+      real(dp), dimension(lnx)             :: zs_dummy
+      type(kdtree_instance)                :: treeinst
+      integer, parameter                   :: n_nearest_kdtree = 100
+      integer, dimension(n_nearest_kdtree) :: link_ids_closest_midpoint
+      integer                              :: ierror
+      integer                              :: i_sample
+      real(dp)                             :: dist_perp, dist_perp_min
+      
+      if (size(xx) /= size(yy) .or. size(xx) /= size(link_ids_closest)) then
+         call mess(LEVEL_ERROR,'find_flowlinks: unmatched input array size')
+      end if
+      
+      link_ids_closest = 0
+      
+      ! Calculate the x,y-coordinates of the midpoints of all the flowlinks
+      do link_id = 1, lnx
+         ka = ln(1,link_id)
+         kb = ln(2,link_id)
+         flowlink_midpoints_x(link_id) = (xz(ka) + xz(kb)) / 2.0_dp
+         flowlink_midpoints_y(link_id) = (yz(ka) + yz(kb)) / 2.0_dp
+      end do
+      
+      number_of_points = size(xx)
+      do i_point = 1, number_of_points
+         
+         ! The k-d tree uses the distance to the midpoints of the flowlinks
+         ! which is not actually what we want (namely the perpendicular distance).
+         ! To solve this, we query the nearest n points (default 10) and then
+         ! do a brute-force search on this (very) small subset
+         call find_nearest_sample_kdtree(treeinst, lnx, 2, flowlink_midpoints_x, flowlink_midpoints_y, zs_dummy, &
+                                         xx(i_point), yy(i_point), n_nearest_kdtree, link_ids_closest_midpoint, &
+                                         ierror, jsferic, dmiss)
+         
+         ! Now loop over the n nearest points to find the one with the shortest perpendicular distance
+         dist_perp_min   = huge(dist_perp_min)
+         do i_sample = 1, n_nearest_kdtree
+            link_id = link_ids_closest_midpoint(i_sample)
+            call perpendicular_distance_to_flowlink(xx(i_point), yy(i_point), link_id, dist_perp)
+            if (dist_perp < dist_perp_min) then
+               link_ids_closest(i_point) = link_id
+               dist_perp_min = dist_perp
+            end if
+         end do
+         
+      end do
+      
+   end subroutine find_flowlinks_kdtree
+   
+   !> Find the flowlinks with the shortest perpendicular distances to the points [xx,yy]
+   !! Brute-force approach: simply check all flowlinks in the entire grid
+   subroutine find_flowlinks_bruteforce(xx, yy, link_ids_closest)
+      use stdlib_kinds, only: dp
+      use MessageHandling, only: mess, LEVEL_ERROR
+      use m_flowgeom, only: lnx, ln, xz, yz
+      use m_sferic, only: jsferic, jasfer3D
+      use geometry_module, only: dlinedis
+      use m_missing, only: dmiss
+      
+      real(dp), dimension(:), intent(in   ) :: xx, yy           !< x,y-coordinates
+      integer,  dimension(:), intent(  out) :: link_ids_closest !< ids of the flowlinks whose midpoints lie closest to the points [xx,yy]
+      
+      integer  :: number_of_points, i_point
+      integer  :: link_id
+      real(dp) :: dist_perp, dist_perp_min
+      
+      if (size(xx) /= size(yy) .or. size(xx) /= size(link_ids_closest)) then
+         call mess(LEVEL_ERROR,'find_flowlinks: unmatched input array size')
+      end if
+      
+      link_ids_closest = 0
+      
+      number_of_points = size(xx)
+      do i_point = 1, number_of_points
+         dist_perp_min   = huge(dist_perp_min)
+         do link_id = 1, lnx
+            call perpendicular_distance_to_flowlink(xx(i_point), yy(i_point), link_id, dist_perp)
+            if (dist_perp < dist_perp_min) then
+               link_ids_closest(i_point) = link_id
+               dist_perp_min = dist_perp
+            end if
+         end do
+      end do
+      
+   end subroutine find_flowlinks_bruteforce
+   
    !> Find the 1-D or boundary flowlink with the shortest perpendicular distance to the point [x,y]
    !! Brute-force approach: simply check all flowlinks in the entire grid
    subroutine find_1D_or_boundary_flowlink_bruteforce(x, y, link_id_closest)
@@ -49,23 +153,16 @@ module m_find_flowlink
       real(dp), intent(in   ) :: x, y            !< x,y-coordinates
       integer,  intent(  out) :: link_id_closest !< id of the flowlink whose midpoint lies closest to the point [x,y]
       
-      integer  :: link_id, ka, kb
+      integer  :: link_id
       real(dp) :: dist_perp, dist_perp_min
       integer  :: ja
-      real(dp) :: xa, ya, xb, yb, xn, yn
       
       link_id_closest = 0
       dist_perp_min   = huge(dist_perp_min)
    
       do link_id = 1, lnx
          if (link_id <= lnx1D .or. link_id > lnxi) then ! Only check 1-D and boundary flowlinks
-            ka = ln(1,link_id)
-            kb = ln(2,link_id)
-            xa = xz(ka)
-            ya = yz(ka)
-            xb = xz(kb)
-            yb = yz(kb)
-            call dlinedis(x, y, xa, ya, xb, yb, ja, dist_perp, xn, yn, jsferic, jasfer3D, dmiss)
+            call perpendicular_distance_to_flowlink(x, y, link_id, dist_perp, ja)
             if (ja == 1) then
                if (dist_perp < dist_perp_min) then
                   link_id_closest = link_id
@@ -76,5 +173,38 @@ module m_find_flowlink
       end do
       
    end subroutine find_1D_or_boundary_flowlink_bruteforce
+   
+   !> Calculate the perpendicular distance from the point [x,y] to a flowlink
+   subroutine perpendicular_distance_to_flowlink(x, y, link_id, perpendicular_distance, ja)
+      use stdlib_kinds, only: dp
+      use m_flowgeom, only: lnx, ln, xz, yz
+      use m_sferic, only: jsferic, jasfer3D
+      use geometry_module, only: dlinedis
+      use m_missing, only: dmiss
+      
+      real(dp),          intent(in   ) :: x, y                   !< Coordinates of the point
+      integer,           intent(in   ) :: link_id                !< id of the flowlink
+      real(dp),          intent(  out) :: perpendicular_distance !< Perpendicular distance from the point [x,y] to a flowlink
+      integer, optional, intent(  out) :: ja                     !< Whether or not (1/0) the computation was possible. If line points 1 and 2
+                                                                 !! coincide, ja==0, and distance is just Euclidean distance between 3 and 1. 
+      integer  :: ka, kb
+      real(dp) :: xa, ya, xb, yb
+      integer  :: ja_
+      real(dp) :: xn, yn
+      
+      ka = ln(1,link_id)
+      kb = ln(2,link_id)
+      xa = xz(ka)
+      ya = yz(ka)
+      xb = xz(kb)
+      yb = yz(kb)
+      
+      call dlinedis(x, y, xa, ya, xb, yb, ja_, perpendicular_distance, xn, yn, jsferic, jasfer3D, dmiss)
+      
+      if (present(ja)) then
+         ja = ja_
+      end if
+            
+   end subroutine perpendicular_distance_to_flowlink
    
 end module m_find_flowlink
