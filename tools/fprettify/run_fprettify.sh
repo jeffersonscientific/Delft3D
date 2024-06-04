ac27623ca79c8ec94ec0eac1dd2727b5f3f435ad
noback <Matthias.Noback@deltares.nl>
2024-06-04 15:11:01 +0200

    Only one file so not the whole command times out

--- tools/fprettify/run_fprettify.sh
+++ tools/fprettify/run_fprettify.sh
@@ -13,4 +13,4 @@ cores="${2:-${cores_available}}"
 echo "Using $cores cores"
 
 find "$dir" -type f -iname "*.f90" -not \( -path "./src/third_party/*" -o -path "./src/third_party_open/*" -o -path "./src/engines_gpl/dflowfm/packages/dflowfm_lib/templates/*" \) \
-    | xargs --max-args 10 --max-procs $(nproc) timeout --foreground -v 10 fprettify --config-file tools/fprettify/.fprettify.rc
+    | xargs --max-args 1 --max-procs $(nproc) timeout --foreground -v 10 fprettify --config-file tools/fprettify/.fprettify.rc
