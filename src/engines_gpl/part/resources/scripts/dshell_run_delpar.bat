345ce519d8d58802ca2f240d9c0e5df3856431b0
arjenmarkusdeltaresnl <arjen.markus@deltares.nl>
2024-09-11 16:22:28 +0200

    Add more context and make sure the script is installed
    As Michelle remarked, a bit more context is useful, but it is also necessary to explicitly install the script. Hence the changes.

--- src/engines_gpl/part/resources/scripts/dshell_run_delpar.bat
+++ src/engines_gpl/part/resources/scripts/dshell_run_delpar.bat
@@ -1,8 +1,15 @@
 @echo off
 
+echo .
+echo D-Particle Tracking - command line interface
+echo .
+echo The path to the "run_delpar" script has been added to
+echo the PATH environment variable for easy access.
+echo .
 echo To run D-Particle Tracking:
 echo - change to the directory containing the input file
 echo - type the command: run_delpar name.inp
+echo .
 
 cd /d %USERPROFILE%
 
