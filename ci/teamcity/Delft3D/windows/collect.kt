b2d97271587ae2dd42b27f5b6ecea9cdff1782c4
huisman <matthijs.huisman@deltares.nl>
2024-12-06 09:06:28 +0100

    Use docker build as dependency for testbenches

--- ci/teamcity/Delft3D/windows/collect.kt
+++ ci/teamcity/Delft3D/windows/collect.kt
@@ -68,7 +68,7 @@ object WindowsCollect : BuildType({
     }
 
     dependencies {
-        dependency(WindowsBuild) {
+        dependency(Delft3DSobek_OssBuilds_BuildOssWin64vs2019if23) {
             snapshot {
                 onDependencyFailure = FailureAction.FAIL_TO_START
                 onDependencyCancel = FailureAction.CANCEL
