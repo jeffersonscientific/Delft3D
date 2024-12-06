ed3af8cd3d89a651cd4996184a6a696ef6de14af
huisman <matthijs.huisman@deltares.nl>
2024-12-06 15:38:26 +0100

    Rename kotlin file
    Remove step ID's
    Fix fileshare path
    use default branch filter
    Fix dockerfile comment

--- ci/teamcity/Delft3D/windows/buildEnvironment.kt
+++ ci/teamcity/Delft3D/windows/buildEnvironment.kt
@@ -3,10 +3,12 @@ package Delft3D.windows
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
 
 import Delft3D.template.*
 
-object WindowsDocker : BuildType({
+object WindowsBuildEnvironment : BuildType({
 
     templates(
         TemplateMergeRequest,
@@ -15,9 +17,9 @@ object WindowsDocker : BuildType({
         TemplateMonitorPerformance
     )
 
-    name = "Docker Build"
+    name = "Delft3D build environment container"
     buildNumberPattern = "%build.vcs.number%"
-    description = "DIMRset Windows build container."
+    description = "Delft3D Windows build container."
 
     vcs {
         root(DslContext.settingsRoot)
@@ -27,13 +29,12 @@ object WindowsDocker : BuildType({
     steps {
         powerShell {
             name = "Get tooling from network share"
-            id = "Get_tooling_from_network_share"
             platform = PowerShellStep.Platform.x64
             workingDir = "ci/dockerfiles"
             scriptMode = script {
                 content = """
                     # Define the source directory
-                    ${'$'}sourceDir = "\\dfs-trusted.directory.intra\dfs\Teamcity\Docker\Windows\dhydro-vs2022"
+                    ${'$'}sourceDir = "\\dfs-trusted.directory.intra\dfs\Teamcity\Docker\Windows\dhydro-vs2019"
                     
                     # Get the current working directory
                     ${'$'}destinationDir = Get-Location
@@ -45,12 +46,10 @@ object WindowsDocker : BuildType({
         }
         script {
             name = "print dockerfile"
-            id = "print_dockerfile"
             scriptContent = """type .\ci\dockerfiles\Dockerfile-dhydro"""
         }
         dockerCommand {
             name = "Docker build dhydro"
-            id = "build_1"
             commandType = build {
                 source = file {
                     path = "ci/dockerfiles/Dockerfile-dhydro"
@@ -66,7 +65,6 @@ object WindowsDocker : BuildType({
         }
         dockerCommand {
             name = "Docker push"
-            id = "Docker_push"
             commandType = push {
                 namesAndTags = """
                     containers.deltares.nl/delft3d-dev/delft3d-buildtools-windows:latest
@@ -79,10 +77,9 @@ object WindowsDocker : BuildType({
     triggers {
         vcs {
             triggerRules = """
-                -:.
                 +:ci/dockerfiles/**
             """.trimIndent()
-            branchFilter = "+:none/task/DEVOPSDSC-332-add-initial-windows-dockerfile"
+            branchFilter = "+:<default>"
         }
     }
 
