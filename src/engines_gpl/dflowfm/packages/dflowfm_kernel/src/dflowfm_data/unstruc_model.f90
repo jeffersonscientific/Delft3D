708bbbb096a71479f8105492c7c6f22e8f2370d7
DIRECTORY\groenenb <Julien.Groenenboom@deltares.nl>
2025-03-31 12:46:11 +0200

    remove Albedo keyword

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_model.f90
@@ -1388,7 +1388,6 @@ contains
       call prop_get(md_ptr, 'physics', 'Tempmax', Tempmax)
       call prop_get(md_ptr, 'physics', 'Tempmin', Tempmin)
       call prop_get(md_ptr, 'physics', 'Allowcoolingbelowzero', Jaallowcoolingbelowzero)
-      call prop_get(md_ptr, 'physics', 'Albedo', albedo)
 
       call prop_get(md_ptr, 'physics', 'Salimax', Salimax)
       call prop_get(md_ptr, 'physics', 'Salimin', Salimin)
@@ -3390,9 +3389,6 @@ contains
          end if
          if (writeall .or. Jaallowcoolingbelowzero /= 0) then
             call prop_set(prop_ptr, 'physics', 'Allowcoolingbelowzero', Jaallowcoolingbelowzero, '0 = no, 1 = yes')
-         end if
-         if (writeall) then
-            call prop_set(prop_ptr, 'physics', 'Albedo', albedo, 'Albedo (reflection) coefficient')
          end if
          if (writeall .or. surftempsmofac > 0.0_dp) then
             call prop_set(prop_ptr, 'physics', 'Surftempsmofac', Surftempsmofac, 'Hor . Smoothing factor for surface water in heatflx comp. (0.0-1.0), 0=no')
