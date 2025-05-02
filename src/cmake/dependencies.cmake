62b06ea274270a4ed8fae16d6b79335af0232804
Hidde Elzinga <hidde.elzinga@deltares.nl>
2025-05-02 13:29:46 +0200

    Removed CMakePresets

--- src/cmake/dependencies.cmake
+++ src/cmake/dependencies.cmake
@@ -1,6 +1,8 @@
-#NetCDF
+# NetCDF
 find_package(NetCDF REQUIRED)
 find_package(netcdf-fortran REQUIRED)
+
+# geo spatial
 find_package(gdal REQUIRED)
 find_package(proj REQUIRED)
 find_package(shapelib REQUIRED)
