27770bdb8460abafc01617ac20648b18f1acea60
Hidde Elzinga <hidde.elzinga@deltares.nl>
2025-06-24 11:48:59 +0200

    Improved regex to include (C) and CPACK_PACKAGE_VENDOR

--- tools/fix_copyright.py
+++ tools/fix_copyright.py
@@ -9,14 +9,18 @@ from pathlib import Path
 import time
 
 year = 2025  # The year to update the copyright to
-year_to_replace = "[12][09][0-2][0-9]"  # The year to replace in the copyright
+year_to_replace = f"(?!{year})\\d{{4}}"  # The year to replace in the copyright
 
 path = Path(".")  # The path to the directory containing the files to update
 exclude_dirs = {"third_party_open", ".git"}  # Directories to exclude from the search
 
+copyright_pattern = r"(?:Copyright|\(C\)|CPACK_PACKAGE_VENDOR)"
+regex_deltares_before = f"(?P<before>{copyright_pattern}.*Deltares.*[-\W+])({year_to_replace})(?P<after>[^-\/].*)"
+regex_deltares_after = f"(?P<before>{copyright_pattern}.*[-\W+])({year_to_replace})(?P<after>[^-\/].*Deltares.*)"
+
 # Create a list of regex patterns to search for the copyright year (only "Deltares" copyrights not third-party)
-regex_to_search = [re.compile(f"(?P<before>Copyright.*Deltares.*[-\W+])({year_to_replace})(?P<after>[^-].*)", re.IGNORECASE),
-                   re.compile(f"(?P<before>Copyright.*[-\W+])({year_to_replace})(?P<after>[^-].*Deltares.*)", re.IGNORECASE)]
+regex_to_search = [re.compile(regex_deltares_before, re.IGNORECASE),
+                   re.compile(regex_deltares_after, re.IGNORECASE)]
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="'\r%(levelname)s: %(message)s")
@@ -29,11 +33,12 @@ file_list = [
 
 start_time = time.time()
 total_files = len(file_list)
+change_count = 0
 
 log.info("Updating copyright year to %d", year)
 
 for idx, file in enumerate(file_list, 1):
-    print(f"\rProcessing file {idx} of {total_files}".rjust(100), end="")
+    print(f"\rProcessing file {idx} of {total_files}: {file.name}".rjust(200), end="")
 
     if not file.is_file():
         continue
@@ -49,9 +54,11 @@ for idx, file in enumerate(file_list, 1):
         if not search_regex.search(file_content):
             continue
 
+        log.info("Updating file: %s", file)
         new_content = re.sub(search_regex, f"\g<before>{year}\g<after>", file_content)
         file.write_text(new_content)
+        change_count += 1
         break
 
 elapsed_time = time.time() - start_time
-print(f"\nProcessing complete. Elapsed time: {elapsed_time:.2f} seconds")
+print(f"\nProcessing complete. Elapsed time: {elapsed_time:.2f} seconds, {change_count} files updated.")
