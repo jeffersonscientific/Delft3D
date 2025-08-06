f40d086a03011530823fe8a7e4fe70064be1a874
huisman <matthijs.huisman@deltares.nl>
2025-08-06 15:20:16 +0200

    fix syntax

--- ci/teamcity/Delft3D/ciUtilities/TestPythonCiTools.kt
+++ ci/teamcity/Delft3D/ciUtilities/TestPythonCiTools.kt
@@ -63,7 +63,7 @@ object TestPythonCiTools : BuildType({
                 module = "ruff"
                 scriptArguments = "format --diff"
             }
-            executionPolicy = ExecutionPolicy.ALWAYS
+            executionMode = BuildStep.ExecutionMode.ALWAYS
         }
         python {
             name = "Run linter"
@@ -77,7 +77,7 @@ object TestPythonCiTools : BuildType({
                 module = "ruff"
                 scriptArguments = "check --output-format=junit --output-file=ruff.xml"
             }
-            executionPolicy = ExecutionPolicy.ALWAYS
+            executionMode = BuildStep.ExecutionMode.ALWAYS
         }
         python {
             name = "Run type checker"
@@ -91,7 +91,7 @@ object TestPythonCiTools : BuildType({
                 module = "mypy"
                 scriptArguments = "ci_tools --junit-xml=mypy.xml"
             }
-            executionPolicy = ExecutionPolicy.ALWAYS
+            executionMode = BuildStep.ExecutionMode.ALWAYS
         }
         python {
             name = "Run unit tests"
@@ -109,7 +109,7 @@ object TestPythonCiTools : BuildType({
                     --cov=.
                 """.trimIndent()
             }
-            executionPolicy = ExecutionPolicy.ALWAYS
+            executionMode = BuildStep.ExecutionMode.ALWAYS
         }
     }
 
