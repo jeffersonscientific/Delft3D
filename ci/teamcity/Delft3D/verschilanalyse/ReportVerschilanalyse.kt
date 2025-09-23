76242fa298e6cf95cfe7f267e926414dc70accc2
steur <bart.steur@deltares.nl>
2025-09-23 12:10:40 +0200

    my mail

--- ci/teamcity/Delft3D/verschilanalyse/ReportVerschilanalyse.kt
+++ ci/teamcity/Delft3D/verschilanalyse/ReportVerschilanalyse.kt
@@ -27,7 +27,7 @@ object ReportVerschilanalyse: BuildType({
         param("env.EMAIL_SERVER", "smtp.directory.intra")
         param("env.EMAIL_PORT", "25")
         param("env.EMAIL_FROM", "black-ops@deltares.nl")
-        param("env.EMAIL_TO", "dflowfm-verschilanalyse@deltares.nl")
+        param("env.EMAIL_TO", "bart.steur@deltares.nl")
     }
 
     vcs {
