138377348e5d701a6f5ef8d0e92044843e4ccb0b
Willem Ottevanger <willem.ottevanger@deltares.nl>
2025-10-03 16:54:20 +0200

    All/task/unst 9107 change netis tests to unit tests (#244)
    * add unit tests to d3d4-suite
    
    * update for first two nefis tests in googletest framework
    
    * Update CMakeLists.txt
    
    * move fortran based nefis tests to unit tests
    
    * add missing dlls to unit test and set gtest work dir
    
    * delete now unit tests
    
    * remove .bak
    
    * do not add dll's
    
    * comment slow tests
    
    * Update all_configuration.cmake
    
    * add format for version string to prevent cutoff at 80 characters
    
    * Update flow2d3d_configuration.cmake
    
    * include unit_test_configuration once at top level for all configurations
    
    * fprettify
    
    * revert order change
    
    * remove do continue loops
    
    * Update tests_nefis_helper.f90
    
    * remove do\+ [0-9]+ style loops
    
    * add reset error count
    
    * combine tests into single file - one for fast and one for slow
    
    * remove test cpp
    
    * fprettify

--- src/cmake/configurations/suites/fm_configuration.cmake
+++ src/cmake/configurations/suites/fm_configuration.cmake
@@ -2,9 +2,9 @@
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/dimr_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/dflowfm_configuration_basic.cmake)
- 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/dwaq_configuration.cmake)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/dflowfm_configuration_basic.cmake)
  
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/components/dwaves_configuration.cmake)
 
